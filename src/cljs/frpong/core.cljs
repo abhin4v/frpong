25ab05ede066854ec714669ac6fa482df110300b
Abhinav Sarkar <abhinav.sarkar@gmail.com>
2013-09-25 22:05:07 +0530

    Cleaned up code, fixed collision detection

--- src/cljs/frpong/core.cljs
+++ src/cljs/frpong/core.cljs
@@ -30,6 +30,8 @@
 ;;                                                 | renderer |
 ;;                                                 +----------+
 
+(defn abs [x] (.abs js/Math x))
+
 (defn positioner [tick-chan vel-chan pos-chan-in pos-chan-out]
   (go-loop
     (let [tick (<! tick-chan)
@@ -40,43 +42,57 @@
 
 (defn collision-detector [width height padding tick-chan pos-chan vel-chan-in vel-chan-out]
   (go-loop
-    (let [tick (<! tick-chan)
+    (let [adjust-v (fn [p v size] 
+                      (cond
+                        (< p padding) (abs v)
+                        (> p (- size padding)) (- (abs v))
+                        :else v))
+          tick (<! tick-chan)
           [vel-x vel-y] (<! vel-chan-in)
           [x y] (<! pos-chan)
           [xn yn] [(+ x (* vel-x tick)) (+ y (* vel-y tick))]
-          vel-xn (if (and (> xn padding) (< xn (- width padding))) vel-x (- vel-x))
-          vel-yn (if (and (> yn padding) (< yn (- height padding))) vel-y (- vel-y))]
+          vel-xn (adjust-v xn vel-x width)
+          vel-yn (adjust-v yn vel-y height)]
       (>! vel-chan-out [vel-xn vel-yn]))))
+
+(defn render-loop [pos-chan vel-chan]
+  (go-loop
+    (let [[x y] (map int (<! pos-chan))
+          vel (<! vel-chan)]
+      (dom/set-text! (dom/by-id "pos") [x y])
+      (dom/set-text! (dom/by-id "vel") vel)
+      (dom/set-attr! (dom/by-id "ball") "cx" x)
+      (dom/set-attr! (dom/by-id "ball") "cy" y))))
+
+(defn game-setup [width height padding frame-chan pos-chan vel-chan]
+  (let [[tick-chan-pos tick-chan-collsion] (h/dup-chan (h/diff-chan frame-chan))
+        [pos-chan-pos pos-chan-render pos-chan-collision] (h/multiplex pos-chan 3)
+        [vel-chan-pos vel-chan-collision vel-chan-render] (h/multiplex vel-chan 3)]
+    (positioner tick-chan-pos vel-chan-pos pos-chan-pos pos-chan)
+    (collision-detector width height padding
+      tick-chan-collsion pos-chan-collision vel-chan-collision vel-chan)
+    [pos-chan-render vel-chan-render]))
+
+(defn game-init [width height padding init-pos init-vel frame-chan]
+  (let [pos-chan (chan 1)
+        vel-chan (chan 1)
+        [pos-chan-render vel-chan-render] 
+          (game-setup width height padding frame-chan pos-chan vel-chan)]
+    (put! pos-chan init-pos)
+    (put! vel-chan init-vel)
+
+    (render-loop pos-chan-render vel-chan-render)))
 
 (defn ^:export init []
   (let [frame-chan (h/frame-chan)
-        [frame-chan1 frame-chan2] (h/dup-chan frame-chan)
-        
-        fps-chan (h/map-chan #(/ 1000 %) (h/diff-chan frame-chan2))
-        
+        [frame-chan-fps frame-chan-game] (h/dup-chan frame-chan)
+
+        fps-chan (h/map-chan #(/ 1000 %) (h/diff-chan frame-chan-fps))
+
         width 300
         height 200
         padding 5
         init-pos [5 100]
-        init-vel [0.1 0.12]
-        
-        [tick-chan-pos tick-chan-collsion] (h/dup-chan (h/diff-chan frame-chan1))
-        
-        pos-chan (chan)
-        [pos-chan-pos pos-chan-render pos-chan-collision] (h/multiplex pos-chan 3)
-
-        vel-chan (chan)
-        [vel-chan-pos vel-chan-collision] (h/dup-chan vel-chan)]
-    (positioner tick-chan-pos vel-chan-pos pos-chan-pos pos-chan)
-    (collision-detector width height padding 
-      tick-chan-collsion pos-chan-collision vel-chan-collision vel-chan)
-    
-    (go (>! pos-chan init-pos))
-    (go (>! vel-chan init-vel))
-    
-    (go-loop
-      (let [[x y] (map int (<! pos-chan-render))]
-        (dom/set-text! (dom/by-id "fps") (<! fps-chan))
-        (dom/set-text! (dom/by-id "pos") [x y])
-        (dom/set-attr! (dom/by-id "ball") "cx" x)
-        (dom/set-attr! (dom/by-id "ball") "cy" y)))))
+        init-vel [0.2 0.22]]
+    (go-loop (dom/set-text! (dom/by-id "fps") (<! fps-chan)))
+    (game-init width height padding init-pos init-vel frame-chan-game)))
